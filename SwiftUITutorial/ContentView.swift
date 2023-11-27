6e6d63a915eb2e918cca0601c14acc8b7e81755a
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-28 01:37:50 +0300

    Text

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -13,7 +13,24 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            //            Text("Tüm metni küçük harf yazılacak".lowercased())
+            //            Text("Tüm metni büyük harf yazılacak".uppercased())
+            //            Text("kelimelerin ilk harfi büyük olacak".capitalized)
+            //                .font(.title)
+            //                .fontWeight(.semibold)
+            //                .bold()
+            //                .underline()
+            //                .underline(true, color: .red)
+            //                .italic()
+            //                .strikethrough()
+            //                .strikethrough(true, color: .red)
+            //                .font(.system(size: 24, weight: .semibold, design: .serif))
+            //                .baselineOffset(50.0)
+            //                .baselineOffset(-50.0)
+            //                .multilineTextAlignment(.center)
+            //                .foregroundColor(.red)
+            //                .frame(width: 200, height: 100, alignment: .center) // Dikdörtgen çerçeve çizer
+            //                .minimumScaleFactor(0.5) // Metin alana sığmayacağı zaman ne kadar küçülmesine izin verilsin 0.5 = %50
         }
         .padding()
     }
