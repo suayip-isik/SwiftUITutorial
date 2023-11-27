4db302befbad35d030a2324f722da49eb3c4879e
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-27 22:14:56 +0300

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Şuayip Işık on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
